23140ca983c7e409f4c67ed182ad02330bb367d1
kyj <yjkyung@hyundai.com>
2026-05-17 23:50:01 +0900

    remove guestbook

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,8 @@ import { Invitation } from "./component/invitation"
 import { Calendar } from "./component/calendar"
 import { Gallery } from "./component/gallery"
 import { Information } from "./component/information"
-import { GuestBook } from "./component/guestbook"
 import { LazyDiv } from "./component/lazyDiv"
 import { ShareButton } from "./component/shareButton"
-import { STATIC_ONLY } from "./env"
 
 /**
  * 메인 애플리케이션 컴포넌트입니다.
@@ -47,8 +45,6 @@ function App() {
         <LazyDiv className="card-group">
           {/* 축의금 및 연락처 정보 섹션 */}
           <Information />
-          {/* 방명록 섹션 (정적 모드가 아닐 때만 표시) */}
-          {!STATIC_ONLY && <GuestBook />}
         </LazyDiv>
 
         {/* 카카오톡/링크 공유 버튼 */}
