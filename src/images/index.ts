3bee00557a591e0e6bb3737ffb94f29c3c844b72
yunjin544 <yunjin544@gmail.com>
2026-07-22 14:54:35 +0900

    photo update

--- src/images/index.ts
+++ src/images/index.ts
@@ -3,14 +3,41 @@ import image1 from "./image1.jpeg"
 import image2 from "./image2.jpeg"
 import image3 from "./image3.jpeg"
 import image4 from "./image4.jpeg"
-// import image5 from "./image5.png"
-// import image6 from "./image6.png"
-// import image7 from "./image7.png"
-// import image8 from "./image8.png"
-// import image9 from "./image9.png"
-// import image10 from "./image10.png"
-// import image11 from "./image11.png"
-// import image12 from "./image12.png"
+import image5 from "./image5.jpeg"
+import image6 from "./image6.jpeg"
+import image7 from "./image7.jpeg"
+import image8 from "./image8.jpeg"
+import image9 from "./image9.jpeg"
+import image10 from "./image10.jpeg"
+import image11 from "./image11.jpeg"
+import image12 from "./image12.jpeg"
+import image13 from "./image13.jpeg"
+import image14 from "./image14.jpeg"
+import image15 from "./image15.jpeg"
+import image16 from "./image16.jpeg"
+import image17 from "./image17.jpeg"
+import image18 from "./image18.jpeg"
+import image19 from "./image19.jpeg"
+import image20 from "./image20.jpeg"
+import image21 from "./image16.jpeg"
+import image22 from "./image17.jpeg"
+import image23 from "./image18.jpeg"
+import image24 from "./image19.jpeg"
+import image25 from "./image20.jpeg"
+import image26 from "./image26.jpeg"
+import image27 from "./image27.jpeg"
+import image28 from "./image28.jpeg"
+import image29 from "./image29.jpeg"
+import image30 from "./image30.jpeg"
+import image31 from "./image31.jpeg"
+import image32 from "./image32.jpeg"
+import image33 from "./image33.jpeg"
+import image34 from "./image34.jpeg"
+import image35 from "./image35.jpeg"
+import image36 from "./image36.jpeg"
+import image37 from "./image37.jpeg"
+
+
 
 /**
  * 메인 커버 이미지
@@ -21,16 +48,31 @@ export const COVER_IMAGE = coverImage
  * 갤러리에 표시될 이미지 목록
  */
 export const GALLERY_IMAGES = [
-  image1,
-  image2,
+  // image2,
   image3,
   image4,
-  // image5,
-  // image6,
-  // image7,
-  // image8,
-  // image9,
-  // image10,
-  // image11,
-  // image12,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+  image28,
+  image29,
+  image30,
+  image31,
+  image32,
+  image33,
+  image34,
+  image35,
+  image36,
+  image37,
+  image1,
 ]
